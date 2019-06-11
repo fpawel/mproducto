008e14d5c9f76177f679851bc94c407b0980f93c
binf1611@gmail.com <fpawel>
2019-06-11 15:43:45 +0300

    save

---
+++ internal/app/catalogue_node.go
@@ -0,0 +1,42 @@
+package app
+
+import (
+	"github.com/powerman/must"
+	"gopkg.in/yaml.v2"
+	"strings"
+)
+
+func ReadCatalogueNodes() (result []CatalogueNode) {
+	b := must.ReadFile("catalogue.yml")
+	must.AbortIf(yaml.Unmarshal(b, &result))
+	return
+}
+
+type CatalogueNode struct {
+	Title string
+	Tags  []string
+	Nodes []CatalogueNode
+}
+
+func (u *CatalogueNode) UnmarshalYAML(unmarshal func(interface{}) error) error {
+
+	var x struct {
+		Title string          `yaml:"title,omitempty"`
+		Tags  []string        `yaml:"tags,omitempty"`
+		Nodes []CatalogueNode `yaml:"nodes,omitempty"`
+	}
+	if err := unmarshal(&x); err != nil {
+		return err
+	}
+	if len(x.Title) == 0 && len(x.Tags) > 0 {
+		x.Title = strings.Title(x.Tags[0])
+		tags := append([]string{}, x.Tags...)
+		for i := range tags[1:] {
+			x.Title += ", " + strings.ToLower(tags[i])
+		}
+	}
+	u.Title = x.Title
+	u.Tags = x.Tags
+	u.Nodes = x.Nodes
+	return nil
+}
